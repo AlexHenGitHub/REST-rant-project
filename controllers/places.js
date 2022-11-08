93b615f525ed1a8cb503f047e04a5f4debdd3755
AlexHenGitHub <108377905+AlexHenGitHub@users.noreply.github.com>
2022-11-08 01:12:07 -0500

    new places can be added in the new page

--- controllers/places.js
+++ controllers/places.js
@@ -1,21 +1,10 @@
 const router = require('express').Router()
+const places = require('../models/places.js')
 
 router.get('/', (req, res) => {
-  let places = [{
-    name: 'H-Thai-ML',
-    city: 'Seattle',
-    state: 'WA',
-    cuisines: 'Thai, Pan-Asian',
-    pic: 'images/food1.jpg'
-  }, {
-    name: 'Coding Cat Cafe',
-    city: 'Phoenix',
-    state: 'AZ',
-    cuisines: 'Coffee, Bakery',
-    pic: 'images/food2.jpg'
-  }]
-  res.render('places/index', { places })
+    res.render('places/index', { places })
 })
+
 
 
 router.get('/new', (req, res) => {
@@ -25,8 +14,20 @@ router.get('/new', (req, res) => {
 
 router.post('/', (req, res) => {
   console.log(req.body)
-  res.send('POST /places')
+  if (!req.body.pic) {
+    // Default image if one is not provided
+    req.body.pic = 'http://placekitten.com/400/400'
+  }
+  if (!req.body.city) {
+    req.body.city = 'Anytown'
+  }
+  if (!req.body.state) {
+    req.body.state = 'USA'
+  }
+  places.push(req.body)
+  res.redirect('/places')
 })
+
 
 
 module.exports = router
